0c23da6624ad32e73ad7aaf2e8f14f9ae4472563
jingfelix <jingfelix@outlook.com>
2024-07-31 19:33:04 +0800

    fix: debug mode
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # larkbot_v2
+
+## Run
+
+Install Dependencies:
+```bash
+pdm install
+```
+
+Debug mode:
+```bash
+python3 src/larkbot_v2/app.py
+```
+
+Production mode:
+```bash
+python3 -m larkbot_v2
+```
--- src/larkbot_v2/app.py
+++ src/larkbot_v2/app.py
@@ -4,16 +4,17 @@ import schedule
 
 from larkbot_v2.bot import LarkLKMLBot
 
-bot = LarkLKMLBot()
-
-
 if __name__ == "__main__":
     from larkbot_v2.logger import logger
 
     logger.setLevel("DEBUG")
 
+    bot = LarkLKMLBot()
     bot.run()
 else:
+
+    bot = LarkLKMLBot()
+
     schedule.every(5).minutes.do(LarkLKMLBot().run)
     schedule.run_all()
 
